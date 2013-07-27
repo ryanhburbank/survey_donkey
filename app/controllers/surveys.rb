46148190c2d01d562fcac73415730b5da06f9c18
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-26 20:07:15 -0500

    The survey taking view and routes now work. The contoller properly creates new database records for each of the responses.

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -4,3 +4,22 @@ get '/survey/:id/results' do
 
   erb :survey_results
 end
+
+get '/survey/:id' do
+  @questions = Survey.find_by_id(params[:id]).questions
+  erb :"/surveys/take_survey"
+end
+
+post '/survey/:id/response' do
+  p params 
+  @survey = Survey.find_by_id(params[:id])
+  @survey.questions.each do |question|
+    response = Response.new(:email => params[:email],
+                            :answer_id => params[:"#{question.id}"])
+
+    response.save
+    p response.errors
+  end
+
+  redirect to('/')
+end
