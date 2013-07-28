12ef1025d3f7030eb233b529a8d90d35edbab9e8
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-28 13:32:38 -0500

    fixed header

--- public/js/application.js
+++ public/js/application.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
 
-  $('#question-textarea').hide();
-  $('#back-to-survey').hide();
-  $('#edit-title-btn').val('Save');
+ //  $('#question-textarea').hide();
+ //  $('#back-to-survey').hide();
+ //  $('#edit-title-btn').val('Save');
   
 
- //  // Makes sure user can't create a survey with an empty title
- //  $('input[type="submit"]').attr('disabled','disabled');
+ // //  // Makes sure user can't create a survey with an empty title
+ // //  $('input[type="submit"]').attr('disabled','disabled');
    
- // $('input[type="text"]').keyup(function() {
- //    if($(this).val() != '') {
- //       $('input[type="submit"]').removeAttr('disabled');
- //    }
- //  });
+ // // $('input[type="text"]').keyup(function() {
+ // //    if($(this).val() != '') {
+ // //       $('input[type="submit"]').removeAttr('disabled');
+ // //    }
+ // //  });
 
  
   
-  $('#edit-title-form').submit(function(updateTitle){
-    updateTitle.preventDefault();
+ //  $('#edit-title-form').submit(function(updateTitle){
+ //    updateTitle.preventDefault();
    
-    var url = $(this).attr('action');
-    var data = $(this).serialize();
+ //    var url = $(this).attr('action');
+ //    var data = $(this).serialize();
 
-    $.post(url, data, function(response){
-      $('#survey-title').html($(response).find('#survey-title').text());
-    });
-  });
+ //    $.post(url, data, function(response){
+ //      $('#survey-title').html($(response).find('#survey-title').text());
+ //    });
+ //  });
 
  //    $('#edit-title-text').hide();
  //    $('#edit-title-btn').hide();
